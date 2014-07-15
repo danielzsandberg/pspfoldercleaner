5c8da34a4bf259930e9faeba9151407d82984f50
Daniel Sandberg <dsandber@andrew.cmu.edu>
2014-07-15 11:32:23 -0400

    Initial commit

---
+++ PSPFolderCleaner/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSPFolderCleaner
+{
+    /// <summary>
+    /// The main entry point for this program.
+    /// </summary>
+    class Program
+    {
+        #region Methods
+
+        /// <summary>
+        /// The main entry method for this program
+        /// </summary>
+        /// <param name="args">User inputted arguments</param>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                GoAndDeleteNonCSFiles(new DirectoryInfo(folderBrowserDialog.SelectedPath));
+
+                Console.WriteLine("Purge complete.");
+            }
+        }
+ 
+        private static void GoAndDeleteNonCSFiles(DirectoryInfo directory)
+        {
+            IEnumerable<FileInfo> files = directory.EnumerateFiles();
+            foreach(FileInfo file in files)
+            {
+                if(file.Extension != ".cs" || file.Extension != ".wxs")
+                {
+                    Console.WriteLine("{0} deleted!", file.FullName);
+                    file.Delete();
+                }
+            }
+
+            IEnumerable<DirectoryInfo> directories = directory.EnumerateDirectories();
+            foreach(DirectoryInfo currentDirectory in directories)
+            {
+                GoAndDeleteNonCSFiles(currentDirectory);
+            }
+        }
+
+        #endregion
+    }
+}
